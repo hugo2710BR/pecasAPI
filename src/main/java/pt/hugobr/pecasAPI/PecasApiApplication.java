7f0a440e442b2a881be0aa31edfb2600d3cb7d8a
hugoBR2710 <hugosoares2710@outlook.pt>
2023-03-25 21:46:06 +0000

    teste

---
+++ src/main/java/pt/hugobr/pecasAPI/PecasApiApplication.java
@@ -0,0 +1,13 @@
+package pt.hugobr.pecasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/hugobr/pecasAPI/PecasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.hugobr.pecasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
